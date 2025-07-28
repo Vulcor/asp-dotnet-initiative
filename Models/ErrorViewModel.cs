4029a5b80a4aabddb0e3dd35ade1ac19199d2dd1
Vulcor <Damnrottlet@gmail.com>
2025-07-28 17:15:22 +0300

    initial commit
    init dotnet new mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace asp_dotnet_initiative.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
